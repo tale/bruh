df9d154ecf90dc24fe98b732d3bdb6a94e9c59f4
Aarnav Tale <aarnav@tale.me>
2022-02-06 00:58:53 -0500

    feat: create new command class

---
+++ src/classes/command.ts
@@ -0,0 +1,26 @@
+type Flag = {
+	name: string
+	longFlag: `--${string}`
+	shortFlag: `-${string}`
+}
+
+type CommandOptions = {
+	name: string
+	aliases: string[],
+	flags: Flag[]
+}
+
+// Flags is any to support our exported array type
+export class Command<Flags = any> {
+	options: CommandOptions
+	executor: (flags: Flags, args: string[]) => Promise<void>
+
+	constructor(options: CommandOptions, executor: (flags: Flags, args: string[]) => Promise<void>) {
+		this.options = options
+		this.executor = executor
+	}
+
+	async execute(flags: unknown, args: string[]) {
+		await this.executor(flags as Flags, args)
+	}
+}
--- src/types/command.ts
+++
@@ -1,4 +0,0 @@
-export type Command = {
-	name: string
-	aliases: string[]
-}
