3b75aee4491511a34c74c1a3037bb693cd562faa
Aarnav Tale <aarnav@tale.me>
2022-07-25 12:48:40 -0400

    chore: cleanup runtime string hooking

--- src/interface/color.ts
+++ src/interface/color.ts
@@ -1,68 +1,70 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+// eslint-disable-next-line no-extend-native
 Object.defineProperties(String.prototype, {
 	bold: {
-		value: function (text: string): string {
-			return `${this ?? ''}\x1b[1m${text}\x1b[0m`
-		},
+		value(text: string): string {
+			return `${this ?? ''}\u001B[1m${text}\u001B[0m`
+		}
 	},
 	dim: {
-		value: function (text: string): string {
-			return `${this ?? ''}\x1b[2m${text}\x1b[0m`
-		},
+		value(text: string): string {
+			return `${this ?? ''}\u001B[2m${text}\u001B[0m`
+		}
 	},
 	black: {
-		value: function (text: string): string {
-			return `${this ?? ''}\x1b[30m${text}\x1b[0m`
-		},
+		value(text: string): string {
+			return `${this ?? ''}\u001B[30m${text}\u001B[0m`
+		}
 	},
 	red: {
-		value: function (text: string): string {
-			return `${this ?? ''}\x1b[31m${text}\x1b[0m`
-		},
+		value(text: string): string {
+			return `${this ?? ''}\u001B[31m${text}\u001B[0m`
+		}
 	},
 	green: {
-		value: function (text: string): string {
-			return `${this ?? ''}\x1b[32m${text}\x1b[0m`
-		},
+		value(text: string): string {
+			return `${this ?? ''}\u001B[32m${text}\u001B[0m`
+		}
 	},
 	yellow: {
-		value: function (text: string): string {
-			return `${this ?? ''}\x1b[33m${text}\x1b[0m`
-		},
+		value(text: string): string {
+			return `${this ?? ''}\u001B[33m${text}\u001B[0m`
+		}
 	},
 	blue: {
-		value: function (text: string): string {
-			return `${this ?? ''}\x1b[34m${text}\x1b[0m`
-		},
+		value(text: string): string {
+			return `${this ?? ''}\u001B[34m${text}\u001B[0m`
+		}
 	},
 	magenta: {
-		value: function (text: string): string {
-			return `${this ?? ''}\x1b[35m${text}\x1b[0m`
-		},
+		value(text: string): string {
+			return `${this ?? ''}\u001B[35m${text}\u001B[0m`
+		}
 	},
 	cyan: {
-		value: function (text: string): string {
-			return `${this ?? ''}\x1b[36m${text}\x1b[0m`
-		},
+		value(text: string): string {
+			return `${this ?? ''}\u001B[36m${text}\u001B[0m`
+		}
 	},
 	white: {
-		value: function (text: string): string {
-			return `${this ?? ''}\x1b[37m${text}\x1b[0m`
-		},
+		value(text: string): string {
+			return `${this ?? ''}\u001B[37m${text}\u001B[0m`
+		}
 	}
 })
 export {} // Exporting a fake module allows us to declare global
 
 declare global {
 	interface String {
-		bold(text: string): string
-		dim(text: string): string
-		black(text: string): string
-		red(text: string): string
-		green(text: string): string
-		yellow(text: string): string
-		blue(text: string): string
-		magenta(text: string): string
-		cyan(text: string): string
-		white(text: string): string
+		bold(text: string): string;
+		dim(text: string): string;
+		black(text: string): string;
+		red(text: string): string;
+		green(text: string): string;
+		yellow(text: string): string;
+		blue(text: string): string;
+		magenta(text: string): string;
+		cyan(text: string): string;
+		white(text: string): string;
 	}
 }
