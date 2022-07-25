2b2660d75100ded66162c22dc35ece2c34e2f294
Aarnav Tale <aarnav@tale.me>
2022-07-25 15:20:57 -0400

    style: don't destructure fs constants

--- src/utils/preflight.ts
+++ src/utils/preflight.ts
@@ -4,17 +4,13 @@ import { access, mkdir } from 'node:fs/promises'
 import { config } from './config'
 import { endMetric, startMetric } from './metrics'
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const { F_OK, R_OK, W_OK } = constants
-
 export async function preflight() {
 	startMetric('preflight')
 	try {
 		// eslint-disable-next-line no-bitwise
-		await access(config.paths.prefix, F_OK | R_OK | W_OK)
+		await access(config.paths.prefix, constants.F_OK | constants.R_OK | constants.W_OK)
 	} catch {
 		await mkdir(config.paths.prefix, { recursive: true })
-		await mkdir(config.paths.tiffy)
 		await mkdir(config.paths.cache)
 		await mkdir(config.paths.installed)
 	}
