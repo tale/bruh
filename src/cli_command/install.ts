85836b7d8c8c09ae26f12ae2733f437ee05a8154
Aarnav Tale <aarnav@tale.me>
2022-07-28 03:41:01 -0400

    feat: use new api files in install

--- src/cli_command/install.ts
+++ src/cli_command/install.ts
@@ -1,9 +1,9 @@
-import { Command, Tree } from 'classes'
+import { Command } from 'classes'
+import { install_database } from 'fs_parser'
 import { log, Prompt } from 'interface'
+import { depend_tree } from 'mod_hack'
 import { constants } from 'node:fs'
 import { access, mkdir } from 'node:fs/promises'
-import { argv0 } from 'node:process'
-import { bruh_formula } from 'types'
 import { config, exit_code } from 'utils'
 
 interface Flags {
@@ -26,10 +26,10 @@ export default new Command<Flags>({
 			short_flag: '-y'
 		}
 	]
-}, async (flags, arguments_) => {
-	if (arguments_.length === 0) {
-		log.error('You must supply at least one package to install')
-		return
+}, async (flags, cli_arguments) => {
+	if (cli_arguments.length === 0) {
+		log.error('No packages supplied. Exiting...')
+		return exit_code.error
 	}
 
 	try {
@@ -37,84 +37,89 @@ export default new Command<Flags>({
 		await access(config.paths.tiffy, constants.R_OK)
 	} catch {
 		log.error('No available package indexes.')
-		log.error('Try running %s to resolve this.', ''.bold(`${argv0} update`))
-		return
+		log.error('Try running %s to resolve this.', ''.bold(`${config.bin_entry} update`))
+		return exit_code.error
 	}
 
 	await mkdir(config.paths.cache, { recursive: true })
-	const { resolved, unresolved } = await Tree.resolve(arguments_)
+	const { resolved, unresolved } = await depend_tree.resolve_flat(cli_arguments)
 
-	const tasks = resolved.map(async formula => {
-		const exists = await InstalledPackage.exists(formula)
-		const tree = await Tree.from(formula)
+	// Check for preinstalled formulas
+	const pre_installed = new Array<string>()
+	const install_checks = resolved.map(async formula => {
+		// Don't run expensive calculations if we already allow reinstalling
+		if (flags.reinstall) {
+			return
+		}
 
-		return {
-			name: tree.formula.name,
-			packages: tree.flatten(),
-			exists
+		if (await install_database.is_installed(formula)) {
+			pre_installed.push(formula.name)
 		}
 	})
 
-	const currentlyInstalled = new Array<string>()
-	const installCandidates = new Array<string>()
-	const downloadRequests = new Array<bruh_formula>()
-	const allViewableDependencies = new Array<string>()
+	await Promise.all(install_checks)
 
-	const results = await Promise.allSettled(tasks)
-	for (const result of results) {
-		if (result.status !== 'fulfilled') {
-			const { name, unresolved: unresolvedDependencies } = result.reason
-			log.error('Cannot resolve the following dependencies for %s: %s', ''.bold(name), ''.dim(unresolvedDependencies.join(' ')))
-			unresolved.push(name)
-			continue
+	// Calculate the pending top level formulas to install
+	const top_level_installs = new Set(resolved.filter(to_keep => {
+		if (pre_installed.includes(to_keep.name)) {
+			return false
 		}
 
-		const { name, packages, exists } = result.value
-		if (exists && !flags.reinstall) {
-			currentlyInstalled.push(name)
-			continue
+		if (cli_arguments.includes(to_keep.name)) {
+			return true
 		}
 
-		installCandidates.push(name)
-		downloadRequests.push(...packages)
+		return false
+	}))
 
-		// Retrieves a nice list of all the dependencies for display
-		const niceDependencies = packages.filter(formula => formula.name !== name)
-			.map(formula => formula.name)
+	// Calculate all the secondary dependencies to install
+	const dependency_installs = new Set(resolved.filter(formula => {
+		if (top_level_installs.has(formula)) {
+			return false
+		}
 
-		allViewableDependencies.push(...niceDependencies)
+		if (pre_installed.includes(formula.name)) {
+			return false
+		}
+
+		return true
+	}))
+
+	// Exit early if nothing is resolved
+	if (resolved.length === 0) {
+		log.warning('Unable to resolve the following packages: %s', ''.dim(unresolved.join(' ')))
+		log.warning('Try running %s to resolve this.', ''.bold(`${config.bin_entry} update`))
+		return exit_code.error
 	}
+
+	// Handle unresolved formulas (not erroring, just warning)
 
 	if (unresolved.length > 0) {
-		const list = ''.dim(unresolved.join(' '))
-		log.warning('Skipping the following packages due to errors: %s', list)
-		log.warning('Try running %s to resolve this.', ''.bold(`${argv0} update`))
+		log.warning('Skipping the following unresolved packages: %s', ''.dim(unresolved.join(' ')))
+		log.warning('Try running %s to resolve this.', ''.bold(`${config.bin_entry} update`))
 	}
 
-	if (currentlyInstalled.length > 0 && !flags.reinstall) {
-		const list = ''.bold(currentlyInstalled.join(','))
+	// This can only be greater than 0 if flags.reinstall is false
+	if (pre_installed.length > 0) {
+		const list = ''.bold(pre_installed.join(', '))
 		log.error('The following packages are already installed: %s', list)
 		log.warning('To reinstall packages, try running %s or enabling the %s flag',
-			''.bold(`${argv0} reinstall`),
+			''.bold(`${config.bin_entry} reinstall`),
 			''.bold('--reinstall (-r)')
 		)
-		return
-	}
 
-	if (resolved.length === 0) {
-		return
-	}
-
-	if (installCandidates.length === 0) {
-		throw exit_code.error
+		return exit_code.error
 	}
 
 	log.blank()
-	log.info('The following new packages will be installed: %s', ''.bold(installCandidates.join(' ')))
 
-	const dependencies = [...new Set(allViewableDependencies)].sort()
-	if (dependencies.length > 0) {
-		log.info('The following additional packages will be installed: %s', ''.dim(dependencies.join(' ')))
+	const printable_top_level_installs = [...top_level_installs].map(formula => formula.name)
+	const printable_dependency_installs = [...dependency_installs].map(formula => formula.name)
+
+	log.info('The following new packages will be installed: %s', ''.bold(printable_top_level_installs.join(', ')))
+
+	if (dependency_installs.size > 0) {
+		log.info('The following additional packages will be installed: %s', ''.dim(printable_dependency_installs.join(' ')))
 	}
 
 	if (!flags.yes) {
@@ -125,7 +130,6 @@ export default new Command<Flags>({
 		}
 	}
 
-	// Messy looking deduplication; but it's fast
-	const deduplicatedDownloads = downloadRequests.filter((formula, index, array) => array.findIndex(subformula => subformula.name === formula.name) === index)
-	// Const downloadTasks = deduplicatedDownloads.map(async formula => {})
+	// Set automatically deduplicates anything that may still be on both
+	const download_list = new Set([...top_level_installs, ...dependency_installs])
 })
