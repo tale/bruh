e27161cf668b1d7bce39aee191bdb97d9cc6b5cb
Aarnav Tale <aarnavtale@icloud.com>
2023-03-24 18:35:11 -0400

    feat: embed gpg signatures

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,9 @@
-import { readFile } from 'node:fs/promises'
+import { mkdir, readFile, rm, writeFile } from 'node:fs/promises'
 import { defineConfig } from 'tsup'
 import { load } from 'js-yaml'
 import simple_git from 'simple-git'
+import { readKey, readMessage, verify } from 'openpgp'
+import { execSync } from 'node:child_process'
 
 const header = `// MIT License - Copyright (c) ${new Date().getFullYear()}, Aarnav Tale.`
 
@@ -25,8 +27,8 @@ const version = raw_tag.trim().replace('v', '') ?? '0' // Replace the v prefix i
 const build_time = new Date()
 const date_stamp = `${build_time.getFullYear()}.${build_time.getMonth() + 1}.${build_time.getDate()}`
 
-const authority = config.author_name === 'Aarnav Tale' ? '(Official Build)' : `(Unofficial Build by ${config.author_name})`
 const bin_identity = `${version}_${date_stamp}_${commit_hash.substring(0, 7)}`
+const authority = await verify_build(bin_identity) ? '(Official Build)' : '(Unofficial Build)'
 
 config.user_agent = config.user_agent
 	.replace('$VER', version)
@@ -60,6 +62,58 @@ export default defineConfig(options => {
 		platform: 'node',
 		sourcemap: 'inline',
 		minify: !options.watch,
+		publicDir: 'gpg',
 		clean: true
 	}
 })
+
+async function verify_build(bin_identity: string) {
+	const result = await fetch('https://keys.openpgp.org/vks/v1/by-email/aarnavtale@icloud.com', {
+		method: 'GET',
+		redirect: 'follow',
+		headers: {
+			'User-Agent': config.user_agent
+		}
+	})
+
+	if (!result.ok) {
+		console.error('Failed to fetch public key from OpenPGP server')
+		return false
+	}
+
+	const key = await readKey({ armoredKey: await result.text() })
+	const keyId = key.getKeyID().toHex()
+
+	await rm('./gpg', { force: true })
+	await mkdir('./gpg', { recursive: true })
+	await writeFile('./gpg/bruh.ver', `${keyId}|${bin_identity}`)
+
+	const commands = [
+		'gpg --sign',
+		'-u', key.getKeyID().toHex(),
+		'--armor',
+		'--output', './gpg/bruh.sig',
+		'./gpg/bruh.ver'
+	]
+
+	execSync(commands.join(' '))
+	const sig = await readFile('./gpg/bruh.sig', 'utf8')
+
+	const message = await readMessage({
+		armoredMessage: sig
+	})
+
+	const request = await verify({
+		message,
+		verificationKeys: key
+	})
+
+	const { verified } = request.signatures[0];
+	try {
+		await verified; // throws on invalid signature
+		return true
+	} catch (e) {
+		console.log('Signature could not be verified: ' + e.message);
+		return false
+	}
+}
