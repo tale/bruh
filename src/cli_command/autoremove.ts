24cb4719ff4d0ee85916cbecef09d05c62479bb4
Aarnav Tale <aarnavtale@icloud.com>
2023-03-25 17:54:55 -0400

    fix: simplify autoremove by using a blacklist for uninstallable packages

--- src/cli_command/autoremove.ts
+++ src/cli_command/autoremove.ts
@@ -2,6 +2,7 @@ import { find_command } from 'cli_command'
 import { build_command } from 'factory_builders'
 import { local_state } from 'fs_parser'
 import { log } from 'interface'
+import { depend_tree } from 'mod_hack'
 import { type bruh_formula_state } from 'types'
 import { exit_code } from 'utils'
 
@@ -13,15 +14,28 @@ export default build_command({
 }, async () => {
 	const installed = await local_state.get_installed()
 	const to_uninstall = new Array<bruh_formula_state>()
+	const cannot_uninstall = new Array<string>()
 
 	for await (const formula of installed) {
 		// This is a user-installed formula, skip it
 		if (formula.linked) {
+			const { resolved } = await depend_tree.resolve_flat([formula.name])
+			cannot_uninstall.push(...resolved.map(formula => formula.name))
+			continue
+		}
+	}
+
+	for await (const formula of installed) {
+		if (cannot_uninstall.includes(formula.name)) {
 			continue
 		}
 
-		const tree = await build_removable_tree(formula, installed)
-		to_uninstall.push(...tree)
+		// Deduplicate the tree
+		if (to_uninstall.some(uninstallable => uninstallable.name === formula.name)) {
+			continue
+		}
+
+		to_uninstall.push(formula)
 	}
 
 	if (to_uninstall.length === 0) {
@@ -32,38 +46,10 @@ export default build_command({
 	log.info('The following packages will be removed: %s', to_uninstall.map(formula => ''.bold(formula.name))
 		.join(' '))
 
+	log.blank()
 	const uninstall = find_command('uninstall')
 	await uninstall?.run({}, to_uninstall.map(formula => formula.name))
 
 	return exit_code.success
 })
-
-// Finds the dependencies of a formula that can be uninstalled and returns them as an array
-async function build_removable_tree(formula: bruh_formula_state, installed: bruh_formula_state[]): Promise<bruh_formula_state[]> {
-	const tree = new Array<bruh_formula_state>()
-
-	const is_dependency = installed.find(installed_formula => {
-		if (installed_formula.dependencies) {
-			return installed_formula.dependencies.includes(formula.name)
-		}
-
-		return false
-	})
-
-	if (!is_dependency) {
-		tree.push(formula)
-	}
-
-	for await (const dependency of formula.dependencies ?? []) {
-		const dependency_entry = installed.find(installed_formula => installed_formula.name === dependency)
-
-		if (!dependency_entry) {
-			continue
-		}
-
-		tree.push(dependency_entry, ...await build_removable_tree(dependency_entry, installed))
-	}
-
-	return tree
-}
 
